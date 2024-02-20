a440f0e7a3ea4ce4f89363b5c0ed0478ee9fe978
preethibilla <preethi98billa@gmail.com>
2024-02-19 23:26:42 -0800

    Updated the code

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const connectDB = require('./config/db');
 
 const app = express();
 // Connect Database
-connectDB()
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('API running'));
 
