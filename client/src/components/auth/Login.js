aee2e4f2cc34ba9cf523fbd0a1060b1e956c8bb8
preethibilla <preethi98billa@gmail.com>
2024-02-27 15:37:46 -0800

    Added code for login redirect

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,10 @@
 import React,{Fragment, useState} from 'react';
-import {Link} from 'react-router-dom';
+import {Link, Navigate} from 'react-router-dom';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import {login} from '../../actions/auth';
 
-const Login = () => {
+const Login = ({login, isAuthenticated}) => {
   const [formData, setFormData] = useState({
     email: '',
     password: '',
@@ -13,8 +16,15 @@ const Login = () => {
     setFormData({...formData, [e.target.name]: e.target.value});
   const onSubmit = async e => {
     e.preventDefault();
-      console.log('SUCCESS');
+    login(email, password);
     };
+
+    if(isAuthenticated){
+      //return <Navigate to="/dashboard" />
+      return <Navigate to="/dashboard" replace />
+    }
+
+
   return (
     <Fragment>
       <h1 className="large text-primary">Sign In</h1>
@@ -50,4 +60,11 @@ const Login = () => {
   )
 };
 
-export default Login
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+export default connect(mapStateToProps, {login})(Login); //Login
