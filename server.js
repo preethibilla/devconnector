ef8b9df2b348cc2c4d39bb9f6ab500a2afcc2eaa
preethibilla <preethi98billa@gmail.com>
2024-03-08 15:30:31 -0800

    Changed package.json

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require('./config/db');
 
+
 const app = express();
 // Connect Database
 connectDB();
@@ -15,6 +16,7 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
